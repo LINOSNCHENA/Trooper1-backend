2b2c26149b61685acfac6b1e392e22d631c4b111
Linos Nchena <linosnchena@gmail.om>
2023-04-06 14:33:50 +0200

    corrected

--- functions/status.js
+++ functions/status.js
@@ -1,7 +1,8 @@
 exports.handler = async (event, context, callback) => {
+  console.log("=============|CHANGING OF FUNCTION|===========")
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ up: true },{ Lusaka: true }, { Prague: "London" },{ Moscow: false },{ Stockholm: true })
+    body: JSON.stringify(({ up: true },{ Lusaka: true }, { Prague: "London" },{ Moscow: false },{ Stockholm: true }))
   }
 }
